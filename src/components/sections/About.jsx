503fdf28e8de58dc1500ee18d164cbc1913c783c
yash-patel268 <yashp2126@gmail.com>
2025-03-09 23:18:42 -0400

    Updated about me section

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -2,15 +2,16 @@ import { RevealOnScroll } from "../RevealOnScroll";
 
 export const About = () => {
 
-    const languages = ["React"];
-    const frameworks = ["Node.js", "Python"]
-    const systemsCMS = []
-    const testing  = []
+    const languages = ["Javascript", "Java", "HTML", "CSS", "Python", "C", "C++", "PHP"];
+    const webTech = ["React.js", "Node.js", "Express.js", "Socket.io"]
+    const cloudTools = ["Docker", "Kubernetes", "GCP", "AWS", "Kafka", "Git", "GitHub"]
+    const databases = ["SQL", "NoSQL", "MongoDB", "MySQL"]
+    const testing  = ["Junit", "Jest"]
 
     return (
         <section id="about" className="min-h-screen flex items-center justify-center py-20">
             <RevealOnScroll>
-                <div className="max-w-5xl mx-auto px-4">
+                <div className="max-w-6xl mx-auto px-4">
                     <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center">About Me</h2>
                 
                 <div className="rounded-xl p-8 border-white/10 border hover:-translate-y-1 transition-all">
@@ -18,7 +19,7 @@ export const About = () => {
                             A Software Engineer with over five years of coding experience. Driven by a passion to tackle complex challenges and work on innovative projects
                         </p>
 
-                        <div className="grid grid-cols-1 md:grid-cols-2 md:grid-cols-3 md:grid-cols-4 gap-6">
+                        <div className="grid grid-cols-1 md:grid-cols-2 md:grid-cols-3 md:grid-cols-4 md:grid-cols-5 gap-6">
                             <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
                                 <h3 className="text-xl font-bold mb-4">Languages</h3>
                                 <div className="flex flex-wrap gap-2">
@@ -31,9 +32,9 @@ export const About = () => {
                             </div>
 
                             <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
-                                <h3 className="text-xl font-bold mb-4">Frameworks & Libraries</h3>
+                                <h3 className="text-xl font-bold mb-4">Web Technologies</h3>
                                 <div className="flex flex-wrap gap-2">
-                                    {frameworks.map((tech, key) => (
+                                    {webTech.map((tech, key) => (
                                         <span key={key} className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
                                             {tech}
                                         </span>
@@ -42,9 +43,20 @@ export const About = () => {
                             </div>
 
                             <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
-                                <h3 className="text-xl font-bold mb-4">Systems, CMS, & Databases</h3>
+                                <h3 className="text-xl font-bold mb-4">Cloud & Tools</h3>
                                 <div className="flex flex-wrap gap-2">
-                                    {systemsCMS.map((tech, key) => (
+                                    {cloudTools.map((tech, key) => (
+                                        <span key={key} className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
+                                            {tech}
+                                        </span>
+                                    ))}
+                                </div>
+                            </div>
+
+                            <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
+                                <h3 className="text-xl font-bold mb-4">Databases</h3>
+                                <div className="flex flex-wrap gap-2">
+                                    {databases.map((tech, key) => (
                                         <span key={key} className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
                                             {tech}
                                         </span>
