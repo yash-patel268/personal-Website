c09bd1bcb55c23b4f6be497c4782759cda62c06b
yash-patel268 <yashp2126@gmail.com>
2025-03-13 14:30:54 -0400

    Fixed mobile screen view

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
     <>
       {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}
 
-      <div className={`min-h-screen  transition-opacity duration-700 ${isLoaded ? "opacity-100" : "opacity-0"} bg-black text-gray=100`}>
+      <div className={`min-h-screen min-w-screen transition-opacity duration-700 ${isLoaded ? "opacity-100" : "opacity-0"} bg-black text-gray=100`}>
         <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
         <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
         <Home />
