b5930e7fc509362e95af541673edd4bc911e275b
yash-patel268 <yashp2126@gmail.com>
2025-03-13 14:06:51 -0400

    Added custom domain

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ export const Navbar = ({menuOpen, setMenuOpen}) => {
 
     return (
         <nav className="fixed top-0 w-full z-40 bg-[rgba(10,10,10,0.8)] backdrop-blur-lg border-b border-white/10 shadow-lg">
-            <div className="max-w-5xl mx-auto px-4">
+            <div className="max-w-screen mx-auto px-4">
                 <div className="flex justify-between items-center h-16">
                     <a href="#home" className="font-mono text-xl font-bold text-white">
                         {" "}Yash<span className="text-blue-500"> Patel</span>{" "}
